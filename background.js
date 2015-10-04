377b28a6c2ed7cdff7ec49d49c1ab6410b9fc345
Roy Falik <roy.falik@utexas.edu>
2015-10-04 18:46:37 -0500

    done. Please report bugs to jeffjc97@gmail.com

--- background.js
+++ background.js
@@ -2,24 +2,22 @@ var millisecondsPerHour = 1000 * 60 * 60;
 var millisecondsPerDay = millisecondsPerHour * 24;
 var always = 0;
 
-// the number of milliseconds to clear for each option
 var timeArray = [millisecondsPerHour, millisecondsPerDay, always];
 var timeToClear;
 var clearDate;
 var default_sites = ['https://www.google.com', 'https://www.facebook.com'];
 
-// adds the specified urls into the user's history
+
 function addUrls(sites) {
 	for(i=0; i<sites.length; i++) {
 		chrome.history.addUrl({url:sites[i]});
 	}
 }
 
-// when the extension icon is clicked, retrieve the user's data (time, urls), clear the history, and add the new urls
 chrome.browserAction.onClicked.addListener(function () {
 	chrome.storage.sync.get('time', function(result) {
 		time = result['time'];
-		// if the time hasn't been set (i.e. a new user)
+		// console.log(time);
 		if(time === undefined) {
 			chrome.storage.sync.set({'time':0, 'urls':default_sites});
 			timeToClear = timeArray[0];
@@ -30,7 +28,6 @@ chrome.browserAction.onClicked.addListener(function () {
 		clearDate = (new Date()).getTime() - timeToClear;
 	})
 
-	// clear the user's history
 	// chrome.browsingData.remove({
 	// 	"since": clearDate,
 	// 	"originTypes": {
@@ -41,8 +38,7 @@ chrome.browserAction.onClicked.addListener(function () {
 	// }, function () {
 	// 	alert("Cleared!");
 	// });
-	
-	// calling addUrls to actually add the urls into the history
+
 	chrome.storage.sync.get('urls', function(result) {
 		urls = result['urls'];
 		// console.log(time);

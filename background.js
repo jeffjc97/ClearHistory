d700d28a7d3f014d188d0fb1ea7a7e4d87707bf9
jeffjc97 <jeffreychang@college.harvard.edu>
2015-10-04 17:06:09 -0400

    starting change to add personalized urls to be added

--- background.js
+++ background.js
@@ -5,30 +5,48 @@ var always = 0;
 var timeArray = [millisecondsPerHour, millisecondsPerDay, always];
 var timeToClear;
 var clearDate;
+var default_sites = ['https://www.google.com', 'https://www.facebook.com'];
 
-chrome.storage.sync.get('time', function(result) {
-	time = result['time'];
-	console.log(time);
-	if(time === undefined) {
-		chrome.storage.sync.set({'time':0});
-		timeToClear = timeArray[0];
+
+function addUrls(sites) {
+	for(i=0; i<sites.length; i++) {
+		chrome.history.addUrl({url:sites[i]});
 	}
-	else {
-		timeToClear = timeArray[time];
-	}
-	clearDate = (new Date()).getTime() - timeToClear;
-})
+}
 
 chrome.browserAction.onClicked.addListener(function () {
-	chrome.browsingData.remove({
-		"since": clearDate,
-		"originTypes": {
-			"unprotectedWeb": true
+	chrome.storage.sync.get('time', function(result) {
+		time = result['time'];
+		// console.log(time);
+		if(time === undefined) {
+			chrome.storage.sync.set({'time':0, 'urls':default_sites});
+			timeToClear = timeArray[0];
 		}
-	}, {
-		"history": true
-	}, function () {
-		alert("Cleared!");
-	});
-	chrome.history.addUrl({url:"https://google.com"})
+		else {
+			timeToClear = timeArray[time];
+		}
+		clearDate = (new Date()).getTime() - timeToClear;
+	})
+
+	// chrome.browsingData.remove({
+	// 	"since": clearDate,
+	// 	"originTypes": {
+	// 		"unprotectedWeb": true
+	// 	}
+	// }, {
+	// 	"history": true
+	// }, function () {
+	// 	alert("Cleared!");
+	// });
+
+	chrome.storage.sync.get('urls', function(result) {
+		urls = result['urls'];
+		// console.log(time);
+		if(urls === undefined) {
+			addUrls(default_sites);
+		}
+		else {
+			addUrls(urls);
+		}
+	})
 })
--- options.js
+++ options.js
@@ -1,22 +1,92 @@
+var urls;
+var time;
+var default_sites = ['https://www.google.com', 'https://www.facebook.com']
+
+function populateSiteList(urls) {
+	console.log("popsitelist called");
+	console.log(urls);
+	for(i=0; i<urls.length; i++) {
+		url_entry = '<li index="' + i + '">' + urls[i] + '</li>';
+		$('#accepted-urls').append(url_entry);
+	}
+}
+
+function validateUrl(input_url) {
+	chrome.history.addUrl({url:input_url}, function() {
+		// check to see if the url is formatted correctly
+		if (chrome.extension.lastError){
+			var errorMsg = chrome.extension.lastError.message;
+			console.log(errorMsg);
+			if (errorMsg == "Url is invalid."){
+				console.log("ERROR!!");
+				$('.invalid-url').show();
+				$('.urlinput').val('');
+			}
+		}
+		// if it is correct, add it to the local storage and display on frontend
+		else {
+			addUrl(input_url);
+		}
+	});
+}
+
+function addUrl(input_url) {
+	// backend
+	urls.push(input_url);
+	chrome.storage.sync.set({'time':time, 'urls':urls}, function() {
+		// frontend
+		url_entry = '<li index="' + urls.length + '">' + input_url + '</li>';
+		$('#accepted-urls').append(url_entry);
+	});
+}
+
+function deleteUrl(input_url) {
+	// backend
+	// frontend
+}
+
 $(document).ready( function() {
 	// alert("yoo");
 	chrome.storage.sync.get('time', function(result) {
 		time = result['time'];
 		console.log(time);
 		if(time === undefined) {
-			chrome.storage.sync.set({'time':0});
-			$('.btn-group').find('[time="0"]').addClass("selected")
+			chrome.storage.sync.set({'time':0, 'urls':default_sites}, function() {
+				$('.btn-group').find('[time="0"]').addClass("selected")
+			});
 		}
 		else {
 			$('.btn-group').find('[time="' + time + '"]').addClass("selected");
 		}
 	})
 
+	chrome.storage.sync.get('urls', function(result) {
+		urls = result['urls'];
+		// kinda bad but fix later
+		if(urls === undefined) {
+			urls = default_sites
+			chrome.storage.sync.set({'time':0, 'urls':default_sites});
+		}
+		populateSiteList(urls);
+	})
+
 	$('.btn').click(function() {
 		$('.btn-group').find('.selected').removeClass('selected');
-		newTime = $(this).attr("time");
-		console.log("New Time: ", newTime);
-		chrome.storage.sync.set({'time':newTime});
+		time = $(this).attr("time");
+		chrome.storage.sync.set({'time':time, 'urls':urls}, function() {
+			console.log("New Time: ", time);
+		});
 		$(this).addClass("selected");
 	})
+
+	 $('.urlinput').keydown(function(event) {
+		if($('.invalid-url').css('display') != 'none') {
+			$('.invalid-url').hide();
+		}
+		if (event.keyCode == 13) {
+			console.log($(this).val());
+			validateUrl($(this).val());
+			return false;
+		 }
+	});
 });
